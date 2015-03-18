c2f9a92aa8d2f7863b49a51b10eda37f09975847
Anirban Bhattacharya <anirbanbhattacharya1234@yahoo.co.in>
2015-03-18 15:28:55 -0500

    code cleaned up

--- logWatcher.js
+++ logWatcher.js
@@ -1,101 +1,106 @@
+//Modules to load
 var fs = require('fs');
-
 var http = require('http');
-
 var url = require('url');
 var io = require('socket.io');
+
+//Variables to declare
+var accessFile='/var/log/apache2/access.log';
+var errorFile='/var/log/apache2/error.log';
+var serverPort=8080;
 var count =0;
 var countErr=0;
 var all='';
 var allError='';
 var mTime='';
 ErrmTime='';
+
  var server = http.createServer(function(request, response){
-        console.log('Connection');
-
-        var path = url.parse(request.url).pathname;
-
-      console.log(path);
-   switch(path){
+        
+	var path = url.parse(request.url).pathname;
+	console.log(path);
+        switch(path){
 
 case '/':
 
         response.writeHead(200, {'Content-Type': 'text/html'});
-        response.write('hello world');
+        response.write("Welcome to Apache Server Monitoring.<br><a href='./monitor'>Click Here to Access The Monitoring Page</a>");
         break;
-case '/user':
-var data = fs.readFileSync('logView.html','utf8')
-      // fs.readFile(__dirname + path, function(error, data){
-console.log('..in user..');
-/* fs.readFile('main.html', 'utf8',function(error, data){
-                    if (error){
-                        response.writeHead(404);
-                        response.write("opps this doesn't exist - 404");
-                    }
-                    else{
-    console.log(data);
-                        response.writeHead(200, {"Content-Type": "text/html"});
-                        response.write(data, "utf8");
-                    }
-                });*/
-//console.log(data);
-                        response.writeHead(200, {"Content-Type": "text/html"});
-                        response.write(data, "utf8");
-                break;
+case '/monitor':
+	var data = fs.readFileSync('logView.html','utf8')
+        response.writeHead(200, {"Content-Type": "text/html"});
+        response.write(data, "utf8");
+        break;
 default:
-response.writeHead(404);
+	response.writeHead(404);
         response.write('Gone case');
         break;
 }
         response.end();
-    });
-
-    server.listen(8080);
-  var socket=io.listen(server);
-socket.on('connection', function(socket){
-  console.log('....new connection..');
-/*setInterval(function(){
-
-        socket.emit('message', {'message': 'hello world'+count});
-count++
-    }, 2000);*/
-fs.watchFile('/var/log/apache2/access.log',act);
-//fs.watchFile('access.txt',act);
-fs.watchFile('/var/log/apache2/error.log',act2);
-
 });
-function act(cur,prev) {
-console.log('Access::'+cur.mtime+'::'+mTime);
-if(cur.mtime!=mTime)
-{
-count++;
 
- all=all+'<br>'+cur.mtime;
- socket.emit('Update', {'message': all,'ErrMessage':allError,'Count':count,'Err':countErr});
-mTime=cur.mtime;
+        server.listen(serverPort);
 
-}
+  	var socket=io.listen(server);  //attach socket on server
+	
+        fs.watchFile(accessFile,callBackAccess);
+	fs.watchFile(errorFile,callBackError);
+
+        //Attach watcher on files on connection to display current status on new connections
+        socket.on('connection', function(socket){
+  	console.log('....new connection..');
+	socket.emit('Update', {'message': all,'ErrMessage':allError,'Count':count,'Err':countErr});
+	
+
+	});
+
+
+////Commented code to tame fs.watchFile
+
+/*function callBackAccess(cur,prev) {
+        console.log('hit..');
+	if(cur.mtime!=mTime)
+	{
+	count++;
+
+ 	all=all+'<br>'+cur.mtime;
+ 	socket.emit('Update', {'message': all,'ErrMessage':allError,'Count':count,'Err':countErr});
+	mTime=cur.mtime;
+
+	}
+	};
+
+function callBackError(cur,prev) {
+
+	if(cur.mtime!=ErrmTime)
+	{
+ 	countErr++;
+	allError=allError+'<br>'+cur.mtime;
+ 	socket.emit('Update', {'message': all,'ErrMessage':allError,'Count':count,'Err':countErr});
+	ErrmTime=cur.mtime;
+	}
 };
-/*function act(event,filename) {
-console.log(filename+':'+event);
- all=all+'<br>'+event+':'+new Date();
- socket.emit('Access', {'message': 'Total Count:'+count+'<br>'+all});
 
-count++;
-};*/
+*/
 
-function act2(cur,prev) {
-console.log('Error::'+cur.mtime+'::'+ErrmTime);
-if(cur.mtime!=ErrmTime)
-{
- countErr++;
-allError=allError+'<br>'+cur.mtime;
- socket.emit('Update', {'message': all,'ErrMessage':allError,'Count':count,'Err':countErr});
-ErrmTime=cur.mtime;
-}
+function callBackAccess(cur,prev) {
+        console.log('hit..');
+	
+	count++;
 
+ 	all=all+'<br>'+cur.mtime;
+ 	socket.emit('Update', {'message': all,'ErrMessage':allError,'Count':count,'Err':countErr});
+	
 
+	
+	};
+function callBackError(cur,prev) {
 
+	
+ 	countErr++;
+	allError=allError+'<br>'+cur.mtime;
+ 	socket.emit('Update', {'message': all,'ErrMessage':allError,'Count':count,'Err':countErr});
+	
 };
 
 
